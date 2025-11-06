32ca4329240449267fb1a4bb90ee21f9b56702ef
a24kimgalgal <a24kimgalgal@inspedralbes.cat>
2025-11-06 09:09:25 +0100

    Arreglada inde.js

--- FitAI/frontend/src/router/index.js
+++ FitAI/frontend/src/router/index.js
@@ -1,16 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import FitAI from '@/pages/FitAI.vue'
 import ModoJuego from '@/pages/ModoJuego.vue'
-import JuegoSolo from '@/pages/JuegoSolo.vue' // 👈 importa tu nuevo componente
+import JuegoSolo from '@/pages/JuegoSolo.vue' 
 import Multiplayer from '@/pages/Multiplayer.vue'
 
 const routes = [
   { path: '/', name: 'Home', component: FitAI },
   { path: '/modo/:ejercicio', name: 'ModoJuego', component: ModoJuego },
-  { path: '/juego-solo/:ejercicio', name: 'JuegoSolo', component: JuegoSolo }, // 👈 nueva ruta
+  { path: '/juego-solo/:ejercicio/:sessionId', name: 'JuegoSolo', component: JuegoSolo }, 
   { path: '/multiplayer/:ejercicio', name: 'Multiplayer', component: Multiplayer },
 ]
-
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
