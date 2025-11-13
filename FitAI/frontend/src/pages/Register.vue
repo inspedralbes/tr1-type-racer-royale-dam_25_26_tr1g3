e4d2e7eadb9bd40b95f60ccfc1f8b7d0829ea9ab
MarcCara <a24marcarmon@inspedralbes.cat>
2025-11-13 10:11:55 +0100

    Cambios en la configuracion del LOGO

--- FitAI/frontend/src/pages/Register.vue
+++ FitAI/frontend/src/pages/Register.vue
@@ -8,7 +8,7 @@
           src="@/assets/LOGO.png" 
           alt="Logo NextRep"
           class="logo-login mb-4"
-          max-height="180"
+          max-height="100"
           contain
         ></v-img>
 
